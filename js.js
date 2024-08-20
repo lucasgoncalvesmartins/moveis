6cc4ea5b5a6231fc9ce57a6a2a3c6527587ab087
Lucas Martins <lucascamargogoncalvesmartins@gmail.com>
2024-08-20 20:11:53 -0300

    modificação js

---
+++ js.js
@@ -0,0 +1,11 @@
+var banners = ["Os melhores do Brasil", "Qualidade e preço baixo"];
+var banner = 0;
+
+function trocaBanner(){
+    banner = (banner + 1) % 2;
+    document.querySelector("h2#mensagem").textContent = banners[banner];
+
+
+}
+
+setInterval(trocaBanner, 2000)
